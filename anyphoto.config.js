0ecacbab08f5f556f7eb3b17be2a50bae6e1f71f
weirui88888 <ruiwei88888@163.com>
2023-09-27 16:08:25 +0800

    feat(package.json): remove engines

--- anyphoto.config.js
+++ anyphoto.config.js
@@ -4,16 +4,8 @@ const anyPhotoConfig = {
   defaultAuthor: 'AnyPhoto',
   canvasSetting: {
     width: 750,
-    fontFamilys: [
-      'Arial',
-      'Times New Roman',
-      'Verdana',
-      'Tahoma',
-      'Courier New',
-      'Helvetica',
-      'Custom'
-    ],
-    customFontFamilyPath: '',
+    fontFamilys: ['Arial', 'Times New Roman', 'Verdana', 'Tahoma', 'Courier New', 'Helvetica', 'Custom'],
+    customFontFamilyPath: '/Users/weirui05/Desktop/Pacifico-Regular.ttf',
     fallbackFontFamilyIndex: 4,
     backgroundColor: '#006666',
     color: '#FFCC66',
